5749e8bbde1cf4f6b688d4097b48f4f1ba59f21b
anth64 <anth64@protonmail.com>
2022-01-30 21:20:08 -0500

    finished lab 2

---
+++ test/calculator.js
@@ -0,0 +1,39 @@
+// Anthony Giugni - 101259556
+// 30-JAN-2022
+// COMP3133 Lab 2
+var calculator = require('../app/calculator.js');
+var assert = require('chai').assert;
+
+describe('calculator.js', () => {
+	// Addition
+	it('pass', () => {
+		assert.equal(calculator.add(5, 2), 7);
+	});
+	it('fail', () => {
+		assert.equal(calculator.add(5, 2), 8);
+	});
+
+	// Subtraction	
+	it('pass', () => {
+		assert.equal(calculator.sub(5, 2), 3);
+	});
+	it('fail', () => {
+		assert.equal(calculator.sub(5, 2), 2);
+	});
+	
+	// Multiplication
+	it('pass', () => {
+		assert.equal(calculator.mul(5, 2), 10);
+	});
+	it('fail', () => {
+		assert.equal(calculator.mul(5, 2), 12);
+	});
+
+	// Division
+	it('pass', () => {
+		assert.equal(calculator.div(10, 2), 5);
+	});
+	it('fail', () => {
+		assert.equal(calculator.div(10, 2), 2);
+	});
+});
